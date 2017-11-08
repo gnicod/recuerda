0d5325fe2593e4a8c9f0e048ddfccd18df164f32
Gaetan Nicod <nicod.ga@gmail.com>
2017-11-08 23:46:58 +0100

    add components

--- webapp/src/components/Home.vue
+++ webapp/src/components/Home.vue
@@ -1,8 +1,12 @@
+<script>
+</script>
+
 <template>
   <div>
     <h4 v-if="authenticated">
         You are logged in!
     </h4>
+    <listlinguee v-if="authenticated" />
     <h4 v-if="!authenticated">
       You are not logged in! Please <a @click="auth.login()">Log In</a> to continue.
     </h4>
---
+++ webapp/src/components/ListLinguee.vue
@@ -0,0 +1,12 @@
+<script>
+import ListLinguee from './components/ListLinguee'
+</script>
+<template>
+    <h1>list linguee</h1>
+</template>
+
+<script>
+  export default {
+    name: 'listlinguee',
+  }
+</script>
